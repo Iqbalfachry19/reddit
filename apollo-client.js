1c4ceb16c432c1005cde8e5a51dfb084b181b173
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-19 14:11:21 +0700

    build complete

---
+++ apollo-client.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+const client = new ApolloClient({
+  uri: 'https://jacarei.stepzen.net/api/waxen-gecko/__graphql',
+  headers: {
+    Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
+  },
+  cache: new InMemoryCache(),
+})
+export default client
